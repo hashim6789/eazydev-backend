73625b87b7e717f816ebee56ebecbea522767aa2
MUHAMMED HASHIM <muhammedhashim6789@gmail.com>
2025-04-02 19:28:06 +0530

    reafactor : The cors configuration is modularized

--- src/presentation/express/configs/cors.config.ts
+++ src/presentation/express/configs/cors.config.ts
@@ -1,8 +1,27 @@
 import cors from "cors";
 import { env } from "./env.config";
 
+/**
+ * List of allowed origins for CORS.
+ */
+const allowedOrigins: string[] = [
+  "http://localhost",
+  "http://localhost:5173",
+  "http://3.111.35.151", // Your actual server IP
+  "http://3.111.35.151:5173",
+];
+
 const corsOptions: cors.CorsOptions = {
-  origin: env.FRONTEND_HOST,
+  origin: (
+    origin: string | undefined,
+    callback: (error: Error | null, allow?: boolean | string) => void
+  ) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, origin); // Allow if origin is undefined (e.g., from a server-side request) or in the allowed list
+    } else {
+      callback(new Error("Not allowed by CORS")); // Deny if origin is not in the allowed list
+    }
+  },
   methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
--- src/presentation/express/settings/app.ts
+++ src/presentation/express/settings/app.ts
@@ -1,8 +1,6 @@
-import cors from "cors";
 import express from "express";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
-
 import { apiRouter } from "../routers";
 
 /**
@@ -10,40 +8,14 @@ import { apiRouter } from "../routers";
  */
 import { connectDB } from "../../../infra/databases/mongoose/connecton";
 import { initializePeerServer } from "./peer";
-import { connectSocket } from "./socket";
+import { setupCors } from "../configs";
 
 const app = express();
 
-/**
- * CORS options for allowing all origins.
- */
-const allowedOrigins: string[] = [
-  "http://localhost",
-  "http://localhost:5173",
-  "http://3.111.35.151", // Add your actual server IP
-  "http://3.111.35.151:5173",
-];
-const corsOptions: cors.CorsOptions = {
-  origin: (
-    origin: string | undefined,
-    callback: (error: Error | null, allow?: boolean | string) => void
-  ) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, origin);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-};
-
 app.use(morgan("dev"));
-app.use(cors(corsOptions));
+app.use(setupCors);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(sessionConfig);
 app.use(cookieParser());
 
 // Database connection setup
@@ -55,8 +27,6 @@ initializePeerServer();
 /**
  * Mounting routes for documentation, user-related, and authentication endpoints.
  */
-// app.use("/", documentsRoutes);
 app.use("/api", apiRouter);
-// app.use("/authenticate", authenticateRoutes);
 
 export { app };
