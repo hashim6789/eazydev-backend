777f4ad6d8fd8271680a632cb3e01238c7107440
MUHAMMED HASHIM <muhammedhashim6789@gmail.com>
2025-04-04 21:04:47 +0530

    testing

--- src/presentation/express/settings/peer.ts
+++ src/presentation/express/settings/peer.ts
@@ -26,11 +26,14 @@ import { env } from "../configs/env.config";
 export const initializePeerServer = (app: Application, server: Server) => {
   const peerServer = ExpressPeerServer(server, {
     path: "/peerjs",
+    // debug: true,
+    proxied: true, // Important if behind a reverse proxy (NGINX, Vercel, etc.)
+    allow_discovery: true, // Allow peers to discover each other
   });
 
   app.use("/peerjs", peerServer);
 
   console.log(
-    `PeerJS server is running on http://${env.DOMAIN}:${env.PEER_PORT}/peerjs`
+    `✅ PeerJS running on http://${env.DOMAIN}:${env.PEER_PORT}/peerjs`
   );
 };
